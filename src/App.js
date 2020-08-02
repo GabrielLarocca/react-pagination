61ec9ac565fa6311b360b0e2324a3670a2d389b5
JordanGMaria <jordangmaria075@gmail.com>
2020-08-02 04:17:44 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -14,6 +14,16 @@ export default function App() {
     { nome: "Andre" },
     { nome: "Camila" },
     { nome: "Sabrina" },
+    { nome: "XXXX" },
+    { nome: "yyyyy" },
+    { nome: "AAAAA" },
+    { nome: "222" },
+    { nome: "555" },
+    { nome: "333" },
+    { nome: "11111" },
+    { nome: "2222" },
+    { nome: "KKKK" },
+    { nome: "ZZZZ" },
   ];
 
   const totalItemsCount = array.length;
@@ -22,14 +32,15 @@ export default function App() {
   const [limit, setlimit] = useState({ inicio: 0, fim: 0 })
 
   return (
-    <div className="App">
+    <div className="App container">
       {array.slice(limit.inicio, limit.fim).map((el, i) => {
         return <p>{el.nome}</p>;
       })}
       <Button
         onChange={(a, b, c) => {
-          console.log(a,b,c)
+          setactivePage(c)
           setlimit({ inicio: a, fim: b })
+          console.log(a,b,c)
         }}
         activePage={activePage}
         itemsCountPerPage={itemsCountPerPage}
@@ -37,7 +48,9 @@ export default function App() {
         colorButton={"#000"}
         colorTextButton={"#fff"}
         colorProgressBar={"#8B0000"}
+        ShowChangeState={false}
       />
+      
     </div>
   );
 }
--- src/components/Pagination/Button.js
+++ src/components/Pagination/Button.js
@@ -3,35 +3,38 @@ import "../style.css";
 
 export default function Button(props) {
 
-  const styles = { 
-      colorButton: props.colorButton ? props.colorButton : "",
-      color: props.colorTextButton ? props.colorTextButton : "",
-      colorProgressBar: props.colorProgressBar ? props.colorProgressBar : "",
+  const pages = props.totalItemsCount / props.itemsCountPerPage;
+  const indexOfLastTodo = props.activePage * props.itemsCountPerPage;
+  const indexOfFirstTodo = indexOfLastTodo - props.itemsCountPerPage;
+
+  const styles = {
+    colorButton: props.colorButton ? props.colorButton : "",
+    color: props.colorTextButton ? props.colorTextButton : "",
+    colorProgressBar: props.colorProgressBar ? props.colorProgressBar : "",
   };
   const onChange = () => {
-    const pages = props.totalItemsCount / props.itemsCountPerPage;
-    const indexOfLastTodo = props.activePage * props.itemsCountPerPage;
-    const indexOfFirstTodo = indexOfLastTodo - props.itemsCountPerPage;
-    props.onChange(indexOfFirstTodo, indexOfLastTodo, pages);
+     props.onChange( props.ShowChangeState ? indexOfFirstTodo : 0, indexOfLastTodo, props.activePage + 1);
   };
 
   return (
-        <div className="container">    
-            <div className="progress progress-moved">
-                <div className="progress-bar"  style={{backgroundColor: styles.colorProgressBar}}>
-                </div>                       
-            </div> 
-        <button
+    <div className="container">
+        <span >Você esta vendo {indexOfFirstTodo} de {props.totalItemsCount} </span>
+      <div className="progress progress-moved">
+        <div
+          className="progress-bar"
+          style={{ backgroundColor: styles.colorProgressBar, width: '50%'}}
+        ></div>
+      </div>
+      <button
         className="stylebutton"
         style={{
-            backgroundColor: styles.colorButton,
-            color: styles.color,
+          backgroundColor: styles.colorButton,
+          color: styles.color,
         }}
         onClick={onChange}
-        >
+      >
         <p>carregar mais {props.itemsCountPerPage} produtos</p>
-        </button>
-        </div>
-
+      </button>
+    </div>
   );
 }
