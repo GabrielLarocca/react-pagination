b38a521b2b580284ce313c3890a69b338ac2f62c
gabss <gabriellarocca1@outlook.com>
2020-08-02 01:18:05 -0300

    laroucas partes

--- src/components/Pagination/Button.js
+++ src/components/Pagination/Button.js
@@ -2,12 +2,10 @@
 import React from 'react'
 import '../style.css'
 
-
 export default function Button (classname){
 
 return(
     <button className={classname ?  'stylebutton' : 'stylebutton'}><p>Carregar Mais total - mostrados produtos</p></button>
-
 
 )
 }
