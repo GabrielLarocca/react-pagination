5fa6cce30cfeb29bdf4d12688e1342d189a721de
gabss <gabriellarocca1@outlook.com>
2020-08-02 04:10:22 -0300

    progress bar

--- src/components/Pagination/Button.js
+++ src/components/Pagination/Button.js
@@ -6,6 +6,7 @@ export default function Button(props) {
   const styles = { 
       colorButton: props.colorButton ? props.colorButton : "",
       color: props.colorTextButton ? props.colorTextButton : "",
+      colorProgressBar: props.colorProgressBar ? props.colorProgressBar : "",
   };
   const onChange = () => {
     const pages = props.totalItemsCount / props.itemsCountPerPage;
@@ -15,15 +16,22 @@ export default function Button(props) {
   };
 
   return (
-    <button
-      className="stylebutton"
-      style={{
-        backgroundColor: styles.colorButton,
-        color: styles.color,
-      }}
-      onClick={onChange}
-    >
-      <p>carregar mais produtos</p>
-    </button>
+        <div className="container">    
+            <div className="progress progress-moved">
+                <div className="progress-bar"  style={{backgroundColor: styles.colorProgressBar}}>
+                </div>                       
+            </div> 
+        <button
+        className="stylebutton"
+        style={{
+            backgroundColor: styles.colorButton,
+            color: styles.color,
+        }}
+        onClick={onChange}
+        >
+        <p>carregar mais {props.itemsCountPerPage} produtos</p>
+        </button>
+        </div>
+
   );
 }
