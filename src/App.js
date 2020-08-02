14dd5a324e16542f190b13f2f32e716bdc46278c
JordanGMaria <jordangmaria075@gmail.com>
2020-08-02 00:49:17 -0300

    remove index.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import {Title, TitleSmall} from './styles'
 import Button from './components/Pagination/Button'
 
 export default function App() {
   return (
     <div className="App">
-       <Title fontSize={50}>
-        Hello Word
-        <span>Texto Menor</span>
-      </Title>
-
-      <TitleSmall>Menor</TitleSmall>
       <Button>Default</Button>
       <Button type="primary">Primary</Button>
       <Button type="error">Error</Button>
       <Button type="success">Success</Button>
       <Button type="warning">Warning</Button>
-
     </div>
   );
 }
