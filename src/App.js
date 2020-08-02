707b8968c593ec90f5081286fe4dc05b7d117b6a
JordanGMaria <jordangmaria075@gmail.com>
2020-08-02 01:16:54 -0300

    inicial exemplo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import Button from './components/Pagination/Button'
+import React from "react";
+import Button from "./components/Pagination/Button";
 
 export default function App() {
+  const array = [
+    { nome: "Jordan" },
+    { nome: "Larocca" },
+    { nome: "Maria" },
+    { nome: "Diego" },
+    { nome: "Fabio" },
+    { nome: "Ana" },
+    { nome: "João" },
+    { nome: "Andre" },
+    { nome: "Camila" },
+    { nome: "Sabrina" },
+  ];
+  const totalItemsCount = array.length;
+  const itemsCountPerPage = 5;
+
   return (
     <div className="App">
-      <Button>Default</Button>
+      <Button
+        array={array}
+        onChange={() => {
+          console.log("Botão selecionado");
+        }}
+        itemsCountPerPage={itemsCountPerPage}
+        totalItemsCount={totalItemsCount}
+        colorButton={"#000"}
+        colorProgressBar={"#8B0000"}
+      >
+        Default
+      </Button>
     </div>
   );
 }
-
